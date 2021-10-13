91eed11e64f5217b5cdf750d0c44656158d6ebcc
hoangluu2001 <hoangthehoaluu@gmail.com>
2021-10-13 11:17:48 +0700

    vue_laravel

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,471 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="hello">
+
+    <table class="table table-bordered-Primary ">
+
+        <thead>
+
+            <tr>
+                <th  scope="col">STT</th>    
+                <th scope="col">Ma Lo</th>
+                <th scope="col">Loai SP</th>
+                <th scope="col">Block</th>
+                <th scope="col">So Lo</th>
+                <th scope="col">DT (m2)</th>
+                <th scope="col">Duong</th>
+                <th scope="col">Goc</th>
+                <th scope="col">View</th>
+                <th scope="col">Huong</th>
+                <th colspan="6">GIA TRAN</th>
+                <th colspan="3">KET QUA DAT CHO UU TIEN</th>
+
+            </tr>
+        </thead>
+
+        
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+
+
+
+            </tr>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+               <td colspan="2">Larry the Bird</td>  
+                <td colspan="2">Larry the Bird</td> 
+                <td colspan="2">Larry the Bird</td> 
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td> 
+                
+
+
+            </tr>
+            <tr>
+                <th scope="row">1</th>  
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">1</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
+
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
+
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
